06b2ff978b6b54f269e03e97c1a56df2092e3e49
Sourcery AI <>
2023-11-27 18:36:31 +0000

    'Refactored by Sourcery'

--- bitcoinBlocksToMLcsv.py
+++ bitcoinBlocksToMLcsv.py
@@ -20,16 +20,14 @@ def hexify(value,zeros):
 		return value.encode('hex').zfill(zeros)
 
 blockchain = Blockchain(sys.argv[1])
-h = 0
 print("height,version, h0, h1, h2, h3, h4, h5, h6, h7, r0, r1, r2, r3, r4, r5, r6, r7, timestamp, bits, nonce")
-for block in blockchain.get_unordered_blocks():
-	header = block.header
-	r = [header.version]
-	r += struct.unpack("<IIIIIIII",binascii.unhexlify(header.previous_block_hash)[::-1])
-	r += struct.unpack("<IIIIIIII",binascii.unhexlify(header.merkle_root)[::-1])
-	r += struct.unpack("<I",bytes(header.hex[68:72]))
-	r += [header.bits]
-	r += [header.nonce]
-	
-	print(str(h)+","+str(r).replace("[","").replace("]",""))
-	h += 1
+for h, block in enumerate(blockchain.get_unordered_blocks()):
+    header = block.header
+    r = [header.version]
+    r += struct.unpack("<IIIIIIII",binascii.unhexlify(header.previous_block_hash)[::-1])
+    r += struct.unpack("<IIIIIIII",binascii.unhexlify(header.merkle_root)[::-1])
+    r += struct.unpack("<I",bytes(header.hex[68:72]))
+    r += [header.bits]
+    r += [header.nonce]
+
+    print(f"{str(h)}," + str(r).replace("[","").replace("]",""))
